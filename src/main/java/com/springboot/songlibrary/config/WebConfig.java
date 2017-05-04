bd2ac9805fd1e389969339b404bc5ae80ae97ddb
Aleksandr Maskalenko <maskalenkoaleksandr@gmail.com>
2017-05-04 13:55:57 +0300

    v3.5

--- src/main/java/com/springboot/songlibrary/config/WebConfig.java
+++ src/main/java/com/springboot/songlibrary/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.springboot.songlibrary.config;
 import com.springboot.songlibrary.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -10,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
 public class WebConfig extends WebSecurityConfigurerAdapter {
 
@@ -20,11 +21,6 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService);
-	}
-
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		super.configure(web);
 	}
 
 	@Override
--- target/classes/static/gulpfile.js
+++ target/classes/static/gulpfile.js
@@ -1,50 +1,62 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-// var mainBowerFiles = require('main-bower-files');
-// var lib    = require('bower-files')();
+var inject = require('gulp-inject');
+var mainBowerFiles = require('main-bower-files');
+// var lib    = require('bower-files-files')();
 // var uglify = require('gulp-uglify');
 
 
 gulp.task('css', function(){
     gulp.src([
-        "./node_modules/bootstrap/dist/css/bootstrap.min.css",
-        "./node_modules/select2/dist/css/select2.min.css",
-        "./app/resource/css/**/*.css"
+        "./app/bower-files/bootstrap.css",
+        "./app/bower-files/select2.css",
+        "./app/css/style.css"
     ])
         .pipe(concat('main.css'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./app/build'));
 });
 
 gulp.task('js', function() {
     gulp.src([
-        "./node_modules/jquery/dist/jquery.min.js",
-        "./node_modules/angular/angular.min.js",
-        "./node_modules/angular-ui-router/release/angular-ui-router.min.js",
-        "./node_modules/select2/dist/js/select2.min.js",
-        "./node_modules/jquery.maskedinput/src/jquery.maskedinput.js",
-        "./app/**/*.js"
+        "./app/bower-files/jquery.js",
+        "./app/bower-files/angular.js",
+        "./app/bower-files/angular-ui-router.js",
+        "./app/bower-files/select2.js",
+        "./app/bower-files/index.js",
+        "./app/js/**/*.js"
     ])
         .pipe(concat('scripts.js'))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./app/build'));
 });
 
 gulp.task('build', function() {
     gulp.start(['css', 'js']);
-    gulp.watch('./app/resource/css/**/*.css', ['css']);
-    gulp.watch('./app/**/*.js', ['js']);
+    // gulp.watch('./app/**/*.css', ['css']);
+    // gulp.watch('./app/**/*.js', ['js']);
 });
 
+gulp.task('index', function () {
 
-// gulp.task('bower', function() {
-//     return gulp.src(mainBowerFiles())
-//         .pipe(gulp.dest('./dist/bower'));
-// });
+    var tpl_src = [
+        "./app/build/**/*.js",
+        "./app/build/**/*.css"];
+
+    return gulp.src('./app/index.html')
+        .pipe(inject(gulp.src(tpl_src)))
+        .pipe(gulp.dest('./'));
+
+});
+
+gulp.task('bower', function() {
+    return gulp.src(mainBowerFiles())
+        .pipe(gulp.dest('./app/bower-files'));
+});
 //
 // gulp.task('files', function () {
 //     gulp.src(lib.ext('js').files)
 //         .pipe(order(['jquery.js', 'angular.js']))
 //         .pipe(concat('scripts.js'))
 //         .pipe(uglify())
-//         .pipe(gulp.dest('./dist/bower'));
+//         .pipe(gulp.dest('./dist/bower-files'));
 //
 // });
